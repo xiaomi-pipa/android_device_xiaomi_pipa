add44dd255a0369204f3fe48ce255181699a7bc4
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    pipa: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_SCREEN_DENSITY := 400
 
 # Kernel
-TARGET_KERNEL_CONFIG += pipa_defconfig vendor/xiaomi/magictime-common.config vendor/xiaomi/pipa.config
+TARGET_KERNEL_CONFIG += pipa_defconfig vendor/xiaomi/magictime-common.config vendor/xiaomi/pipa.config vendor/debugfs.config
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
