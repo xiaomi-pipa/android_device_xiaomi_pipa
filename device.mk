0bbfb8069298723905af1e1dc158697679436b3d
LuK1337 <priv.luk@gmail.com>
2025-06-21 13:44:53 +0200

    pipa: Enable QTI recovery minui backend
    Change-Id: Ia6cd1edeb5f4b1512a1345b1fec51c11129ed068

--- device.mk
+++ device.mk
@@ -51,6 +51,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
 
+# Recovery
+$(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
